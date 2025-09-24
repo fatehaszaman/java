22ac211c8334f21e13b93e163ae819793a3036f4
fatehaszaman <168937738+fatehaszaman@users.noreply.github.com>
2025-09-24 15:41:52 -0400

    KthLargestElementInAnArray215.java

---
+++ Leetcode/KthLargestElementInAnArray215.java
@@ -0,0 +1,17 @@
+// Leetcode question number 215
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();
+    }
+}
